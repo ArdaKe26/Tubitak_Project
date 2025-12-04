08b3a332416b0bd2a0d02d768088ef11ff3561c2
ArdaKe26 <arda.kurtulus26@gmail.com>
2025-12-04 14:56:24 +0300

    password resetting

--- app/login_user.tsx
+++ app/login_user.tsx
@@ -3,6 +3,7 @@ import {
   createUserWithEmailAndPassword,
   getAuth,
   onAuthStateChanged,
+  sendPasswordResetEmail,
   signInWithEmailAndPassword,
   signOut,
 } from "@firebase/auth";
@@ -12,9 +13,13 @@ import {
   Alert,
   Button,
   Image,
+  Keyboard,
+  Modal,
+  Pressable,
   StyleSheet,
   Text,
   TextInput,
+  TouchableWithoutFeedback,
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -36,7 +41,28 @@ export default function LoginScreen(): React.ReactElement {
   const [user, setUser] = useState<any>(null);
   const [isLogin, setIsLogin] = useState(true);
 
+  const [resetModalVisible, setResetModalVisible] = useState(false);
+  const [resetEmail, setResetEmail] = useState("");
+
   const auth = getAuth(app);
+
+  const handleResetPassword = async () => {
+    if (!resetEmail) {
+      Alert.alert("Uyarı", "Lütfen email adresinizi girin.");
+      return;
+    }
+
+    try {
+      await sendPasswordResetEmail(auth, resetEmail);
+      Alert.alert(
+        "Mail Gönderildi",
+        "Şifre sıfırlama bağlantısı email adresinize gönderildi."
+      );
+      setResetModalVisible(false); // popup kapanır
+    } catch (err: any) {
+      Alert.alert("Hata", err.message);
+    }
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (u) => {
@@ -70,62 +96,125 @@ export default function LoginScreen(): React.ReactElement {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.authContainer}>
-        <View style={styles.header}>
-          <Image
-            resizeMode="contain"
-            style={styles.headerImg}
-            source={require("../assets/images/icon.png")}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.authContainer}>
+          <View style={styles.header}>
+            <Image
+              resizeMode="contain"
+              style={styles.headerImg}
+              source={require("../assets/images/icon.png")}
+            />
+            <Text style={{ fontSize: 31, fontWeight: "700", color: "#075eec" }}>
+              Kullanıcı
+            </Text>
+            <Text style={styles.subtitle}>Tarımda Teknolojiyi Keşfedin!!</Text>
+          </View>
+
+          <Text style={styles.inputLabel}>Email</Text>
+          <TextInput
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            keyboardType="email-address"
+            placeholder="ornek@gmail.com"
+            placeholderTextColor="#6b7280"
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
           />
-          <Text style={{ fontSize: 31, fontWeight: "700", color: "#075eec" }}>
-            Kullanıcı
-          </Text>
-          <Text style={styles.subtitle}>Tarımda Teknolojiyi Keşfedin!!</Text>
-        </View>
 
-        <Text style={styles.inputLabel}>Email</Text>
-        <TextInput
-          autoCorrect={false}
-          clearButtonMode="while-editing"
-          keyboardType="email-address"
-          placeholder="ornek@gmail.com"
-          placeholderTextColor="#6b7280"
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          autoCapitalize="none"
-        />
-
-        <Text style={styles.inputLabel}>Şifre</Text>
-        <TextInput
-          autoCorrect={false}
-          clearButtonMode="while-editing"
-          placeholder="********"
-          placeholderTextColor="#6b7280"
-          secureTextEntry={true}
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-        />
-
-        <View style={styles.buttonContainer}>
-          <Button
-            title={isLogin ? "Giriş Yap" : "Kayıt ol"}
-            onPress={handleAuthentication}
-            color="#fff"
+          <Text style={styles.inputLabel}>Şifre</Text>
+          <TextInput
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            placeholder="********"
+            placeholderTextColor="#6b7280"
+            secureTextEntry={true}
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
           />
-        </View>
+          <Pressable
+            onPress={() => {
+              setResetModalVisible(true);
+            }}
+          >
+            {({ pressed }) => (
+              <Text
+                style={[
+                  styles.passwordForgetText,
+                  { color: !pressed ? "#3498db" : "#144c88ff" }, // basılıyken biraz koyulaşır
+                ]}
+              >
+                Şifrenizi mi unuttunuz?
+              </Text>
+            )}
+          </Pressable>
 
-        <View style={styles.bottomContainer}>
-          <Text style={styles.toggleText} onPress={() => setIsLogin(!isLogin)}>
-            {isLogin
-              ? "Hesabınız Yok mu? Hemen Oluşturun"
-              : "Hesabınız Var Mı? Giriş Yapın"}
-          </Text>
+          <View style={styles.buttonContainer}>
+            <Button
+              title={isLogin ? "Giriş Yap" : "Kayıt ol"}
+              onPress={handleAuthentication}
+              color="#fff"
+            />
+          </View>
+
+          <View style={styles.bottomContainer}>
+            <Text
+              style={styles.toggleText}
+              onPress={() => setIsLogin(!isLogin)}
+            >
+              {isLogin
+                ? "Hesabınız Yok mu? Hemen Oluşturun"
+                : "Hesabınız Var Mı? Giriş Yapın"}
+            </Text>
+          </View>
+
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={resetModalVisible}
+            onRequestClose={() => setResetModalVisible(false)}
+          >
+            <View style={styles.modalOverlay}>
+              <View style={styles.modalContainer}>
+                <Text style={styles.modalTitle}>Şifre Sıfırlama</Text>
+                <Text style={styles.modalDesc}>
+                  Şifre sıfırlama bağlantısını göndereceğimiz email adresini
+                  girin:
+                </Text>
+
+                <TextInput
+                  placeholder="ornek@gmail.com"
+                  value={resetEmail}
+                  onChangeText={setResetEmail}
+                  style={styles.modalInput}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+
+                <View style={styles.modalButtons}>
+                  <Pressable
+                    onPress={() => setResetModalVisible(false)}
+                    style={styles.modalCancel}
+                  >
+                    <Text style={styles.modalCancelText}>İptal</Text>
+                  </Pressable>
+
+                  <Pressable
+                    onPress={handleResetPassword}
+                    style={styles.modalConfirm}
+                  >
+                    <Text style={styles.modalConfirmText}>Gönder</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </View>
+          </Modal>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -201,5 +290,64 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginTop: -30,
     borderRadius: 30,
+  },
+  passwordForgetText: {
+    textDecorationLine: "underline",
+    color: "#3498db",
+    marginTop: 6,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContainer: {
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    padding: 20,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#075eec",
+    marginBottom: 10,
+  },
+  modalDesc: {
+    fontSize: 14,
+    color: "#555",
+    marginBottom: 15,
+  },
+  modalInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 20,
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    gap: 10,
+  },
+  modalCancel: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  modalCancelText: {
+    color: "#888",
+    fontSize: 15,
+  },
+  modalConfirm: {
+    backgroundColor: "#075eec",
+    paddingVertical: 10,
+    paddingHorizontal: 18,
+    borderRadius: 10,
+  },
+  modalConfirmText: {
+    color: "#fff",
+    fontWeight: "700",
+    fontSize: 15,
   },
 });
